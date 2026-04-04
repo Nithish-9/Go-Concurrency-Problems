491a977b0939b46e6c91f9380aa955bb2086b5ad
NITHISH <nithishanusri0@gmail.com>
2026-04-04 22:06:24 +0530

    Retry with Backoff

--- problems/RateLimiterTokenBucket/main.go
+++ problems/RateLimiterTokenBucket/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,15 +13,24 @@ type data struct {
 }
 
 func main() {
-	requests := []int{1, 1, 2, 2, 2, 3, 3, 3}
+	requests := [][]int{{1, 2}, {2, 1e9}} // first value -> task, second value -> num of times it fails
 	maxRequests := 2
 
 	ans := make([]string, len(requests))
 	parentCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	tokenChan := make(chan struct{}, maxRequests)
-	requestChan := make(chan int)
-	outChan := make(chan data)
+	requestChan := make(chan []int)
+	allowDropChan := make(chan data)
+
+	numOfWorkers := 3
+	jobChan := make(chan []int, 3)
+	processChan := make(chan string, numOfWorkers)
+	wg := sync.WaitGroup{}
+	for i := 0; i < numOfWorkers; i++ {
+		wg.Add(1)
+		go processAllowedRequests(jobChan, processChan, &wg)
+	}
 
 	// Token Refiller Channel
 	go func() {
@@ -42,40 +52,79 @@ func main() {
 	// Incoming Requests Channel
 	go func() {
 		defer close(requestChan)
-		for index := range requests {
+		for index, inner := range requests {
 			select {
 			case <-parentCtx.Done():
 				return
 			default:
-				requestChan <- index
+				requestChan <- []int{index, inner[0], inner[1]}
 				time.Sleep(time.Second / time.Duration(maxRequests))
 			}
 		}
 	}()
 
-	// process the incoming requests
+	// allow/drop the incoming requests
 	go func() {
-		defer close(outChan)
-		for val := range requestChan {
+		defer close(allowDropChan)
+		defer close(jobChan)
+		for it := range requestChan {
 			select {
 			case <-parentCtx.Done():
 				return
 			case <-tokenChan:
-				outChan <- data{
-					index: val,
+				allowDropChan <- data{
+					index: it[0],
 					value: "ALLOW",
 				}
+				jobChan <- it
 			default:
-				outChan <- data{
-					index: val,
+				allowDropChan <- data{
+					index: it[0],
 					value: "DROP",
 				}
 			}
 		}
 	}()
 
-	for it := range outChan {
-		ans[it.index] = it.value
+	go func() {
+		for it := range allowDropChan {
+			ans[it.index] = it.value
+		}
+		fmt.Println(ans)
+	}()
+
+	go func() {
+		for it := range processChan {
+			fmt.Println(it)
+		}
+	}()
+
+	wg.Wait()
+	close(processChan)
+}
+
+func processAllowedRequests(jobChan <-chan []int, processChan chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	maxRetries := 2
+	for task := range jobChan {
+		fmt.Println("Request Processing for ", task[1])
+		attempt := 0
+		var status string
+		for {
+			if attempt >= task[2] {
+				status = "SUCCESS"
+				break
+			}
+
+			if attempt == maxRetries {
+				status = "FAILED"
+				break
+			}
+
+			time.Sleep(time.Duration(1<<attempt) * time.Second)
+
+			attempt++
+		}
+		processChan <- fmt.Sprintf("Task %d %s", task[1], status)
 	}
-	fmt.Println(ans)
 }
